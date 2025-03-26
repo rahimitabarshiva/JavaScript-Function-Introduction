b79d3c6acf09a51086cc416ec2d61cc39018f332
Shiva Rahimitabar 🙋‍♀️👩‍💻 <rahimitabarshiva@gmail.com>
2025-03-26 15:45:03 +0330

    Developed by : Shiva Rahimitabar
    Date : 03/26/2025

---
+++ assets/JavaScript/Shiva.js
@@ -0,0 +1,15 @@
+function Shiva1() {
+    console.log(`Hello from JavaScript!!!`)
+}
+function Shiva2() {
+    alert(`This is a function exercise!!!!`)
+}
+function Shiva3() {
+    prompt(`Enter your age, please.`)
+}
+function Shiva4() {
+    document.write(`<pre>function structure is like this : <br> function name(params) {<br>Write what this function does, here!<br>}</pre>`)
+}
+function Shiva5() {
+    document.title = 'Title is changed by a function!'
+}
